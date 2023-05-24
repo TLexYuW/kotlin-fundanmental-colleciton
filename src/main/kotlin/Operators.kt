ef9cdaa5a89e75ef3cbf5d03b29b744a4d9b5251
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-05-24 18:31:57 +0800

    update operators

--- src/main/kotlin/Operators.kt
+++ src/main/kotlin/Operators.kt
@@ -1,3 +1,9 @@
 fun main() {
-   
+    var x = 10
+    val y = 3
+    println("x + y = ${x + y}")
+    println("x - y = ${x - y}")
+    println("x * y = ${x * y}")
+    println("x / y = ${x / y}")
+    println("x % y = ${x % y}")
 }
