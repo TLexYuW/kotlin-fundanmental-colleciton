3360f0d2bd21da902d2b60b4bf4d2178e01901ea
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-12-29 17:46:10 +0800

    pre-work

--- src/main/kotlin/other_resources/_11_oop/lazy/Person.kt
+++ src/main/kotlin/other_resources/_11_oop/lazy/Person.kt
@@ -4,10 +4,10 @@ fun main(args: Array<String>) {
     val p1 = Person("Alex", "EEEErrrrr", 2222)
     val p2 = Person("QQQQ", "lllll", 1111)
     val p3 by lazy {
-        Person("Person1", "LastName", 1000)
+        Person("Person3", "LastName", 1000)
     }
 
-    println(p3.firstName)
+//    println(p3.firstName)
 }
 
 class Person(var firstName: String, var lastName: String, var age: Int) {
