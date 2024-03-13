11a1bbc1b64de39861381fb48f12a1f0e46cb6de
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-03-13 23:59:29 +0800

    update to extensions

--- src/main/kotlin/ref_official/visibility_modifiers/Example.kt
+++ src/main/kotlin/ref_official/visibility_modifiers/Example.kt
@@ -3,12 +3,25 @@ package ref_official.visibility_modifiers
 /**
  * @author : Lex Yu
  */
+private fun privateTopLevelFunction() {
+    println("This is a private top-level function")
+}
 class Example {
+    private var privateProperty: String = "Private Property"
+
+    fun getPrivateProperty(): String {
+        return privateProperty
+    }
+
+    fun setPrivateProperty(value: String) {
+        privateProperty = value
+    }
+
     private fun exampleFoo() {
-        println("private foo()")
+        println("private exampleFoo()")
     } // visible inside example.kt
     public fun exampleFoo2() {
-        println("private foo()")
+        println("public exampleFoo2()")
     } // visible inside example.kt
 
     public var bar: Int = 5 // property is visible everywhere
@@ -16,3 +29,4 @@ class Example {
 
     internal val baz = 6    // visible inside the same module
 }
+
--- src/main/kotlin/ref_official/visibility_modifiers/Main.kt
+++ src/main/kotlin/ref_official/visibility_modifiers/Main.kt
@@ -1,12 +1,14 @@
 package ref_official.visibility_modifiers
 
-
 /**
  * @author : Lex Yu
  */
 fun main() {
+
     val e = Example()
 //    e.exampleFoo() // Cannot access 'exampleFoo': it is private in 'Example'
-    e.exampleFoo2()
     e.baz
+    e.exampleFoo2()
+
+    println(e.getPrivateProperty())
 }
