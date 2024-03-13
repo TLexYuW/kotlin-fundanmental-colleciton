17317886eabd4cbf57f1d6633226d9e4f9a0990b
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-03-13 18:31:58 +0800

    Create Demo.kt

---
+++ src/main/kotlin/ref_official/coroutine/Demo.kt
@@ -0,0 +1,16 @@
+package ref_official.coroutine
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * @author : Lex Yu
+ */
+fun main() = runBlocking { // this: CoroutineScope
+    launch { // launch a new coroutine and continue
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+    }
+    println("Hello") // main coroutine continues while a previous one is delayed
+}
